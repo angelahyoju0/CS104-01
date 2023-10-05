8c85f68014b884a58ed5c8bbaba9753c88e42d23
angelahyoju0 <146225964+angelahyoju0@users.noreply.github.com>
2023-10-05 08:32:19 -0400

    This is my Input and conditions program

---
+++ conditions.py
@@ -0,0 +1,21 @@
+# CS104
+
+# Angela An
+
+# conditions.py
+
+temp = "xyz"
+while temp != "end":
+        temp = int(input("Please enter the current tempurature: "))
+        if temp > (90):
+                print("Wear Shorts")
+        elif (91) > temp > (70):
+                print("Short sleeves are fine")
+        elif (71) > temp > (50):
+                print("Wear a jacket")
+        elif (51) > temp > (32):
+                print("Wear a heavy coat")
+        else:
+                print("Stay Inside")
+                
+
